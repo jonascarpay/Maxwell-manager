83bc7719fc67320ad994d941c6ea9ab06ab04512
Otis Carpay <otis@Oma-Kers.fritz.box>
2015-07-06 16:06:56 +0200

    Issuetabel toegevoegd

--- client/issues/single.js
+++ client/issues/single.js
@@ -3,3 +3,13 @@ Template.issueSingle.helpers({
         return this.articles.map(function(id) {return Articles.findOne(id)});
     }
 });
+
+Template.issueArticle.rendered = function() {
+	console.log(this)
+}
+
+Template.issueArticle.helpers({
+	repeater: function() {
+		return new Array(this.pages - 1).fill(0)
+	}
+})
